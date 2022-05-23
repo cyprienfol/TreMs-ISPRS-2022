d61710b10b0e5acfb6166525328fbc2739f62032
Fol  Cyprien <cfol@ethz.ch>
2022-05-23 20:19:05 +0200

    Revert "Revert "adding point cloud ptree 1.7""
    This reverts commit 0de51b1d7def95272ce35dc58f16a54681ed0612.

---
+++ point-clouds-potree/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1711058,
+    "boundingBox": {
+        "lx": 2676526.9085683824,
+        "ly": 1246179.6822805405,
+        "lz": 0.1024322509765625,
+        "ux": 2676530.118748665,
+        "uy": 1246182.892460823,
+        "uz": 3.312612533569336
+    },
+    "tightBoundingBox": {
+        "lx": 2676526.90856838,
+        "ly": 1246179.68228054,
+        "lz": 0.10243224999999999,
+        "ux": 2676528.96705341,
+        "uy": 1246181.45896816,
+        "uz": 3.31261253
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.02780097723007202,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
